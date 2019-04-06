11bfe1ffd44832a160924b58c02ef20c30c4b2bd
Anjan Srihari <anjansrihari14@gmail.com>
2019-04-06 16:05:58 +0530

    fnv destupefied

--- c/fnv_hash.c
+++ c/fnv_hash.c
@@ -1,36 +1,18 @@
 #include <stdio.h>
-#include <math.h>
-#include <string.h>
 #include <stdint.h>
 #include <stdlib.h>
-#define BYTESIZE 256
-#define FNV_PRIME 16777619
-#define FNV_OFFSET 0x811c9dc5
 
-uint32_t fnv_int(uint32_t n){
-    uint32_t hash = FNV_OFFSET;
-    for (int i = 0; i < 4; i++){
-        hash = hash ^ ((n%(int)pow(BYTESIZE,i+1))/(int)pow(BYTESIZE,i));
-        hash = hash * FNV_PRIME;
-    }
-    return hash;
-}
+#define FNV_PRIME32 16777619
+#define FNV_OFFSET32 0x811c9dc5
 
-uint32_t fnv_long(uint64_t n){
-	uint32_t hash = FNV_OFFSET;
-	for (int i = 0; i < 8; i++){
-		hash = hash ^ ((n%(int)pow(BYTESIZE,i+1))/(int)pow(BYTESIZE,i));
-		hash *= FNV_PRIME;
+uint32_t fnv_hash(const void *key, size_t len){
+	uint32_t hash = FNV_OFFSET32;
+	const uint8_t *data = (const uint8_t *)key;
+	while (len > 0){
+		hash = hash ^ *data;
+		hash = hash * FNV_PRIME32;
+		data += 1;
+		len--;
 	}
 	return hash;
 }
-
-uint32_t fnv_string(char *a){
-	uint32_t hash = FNV_OFFSET;
-	for (int i = 0; i < strlen(a); i++){
-		hash = hash ^ a[i];
-		hash *= FNV_PRIME;
-	}
-	return hash;
-}
-//for structures and classes use fnv generic cpp
